091f7c8297743f8db59a84789044e76eaa69443f
cedarglass <166680116+cedarglass@users.noreply.github.com>
2026-02-22 12:06:02 +0800

    Update Storage to support a Remark field

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -28,6 +28,7 @@ class JsonAdaptedPerson {
     private final String name;
     private final String phone;
     private final String email;
+    private final String remark;
     private final String address;
     private final List<JsonAdaptedTag> tags = new ArrayList<>();
 
@@ -37,10 +38,11 @@ class JsonAdaptedPerson {
     @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
             @JsonProperty("email") String email, @JsonProperty("address") String address,
-            @JsonProperty("tags") List<JsonAdaptedTag> tags) {
+            @JsonProperty("remark") String remark, @JsonProperty("tags") List<JsonAdaptedTag> tags) {
         this.name = name;
         this.phone = phone;
         this.email = email;
+        this.remark = remark;
         this.address = address;
         if (tags != null) {
             this.tags.addAll(tags);
@@ -54,6 +56,7 @@ class JsonAdaptedPerson {
         name = source.getName().fullName;
         phone = source.getPhone().value;
         email = source.getEmail().value;
+        remark = source.getRemark().value;
         address = source.getAddress().value;
         tags.addAll(source.getTags().stream()
                 .map(JsonAdaptedTag::new)
@@ -95,7 +98,10 @@ class JsonAdaptedPerson {
         }
         final Email modelEmail = new Email(email);
 
-        final Remark modelRemark = new Remark(""); //TODO: Implement parsing and marshalling in the storage commit.
+        if (remark == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Remark.class.getSimpleName()));
+        }
+        final Remark modelRemark = new Remark(remark);
 
         if (address == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Address.class.getSimpleName()));
